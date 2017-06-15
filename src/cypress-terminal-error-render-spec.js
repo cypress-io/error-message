89e15c43f244779f6ba1faf8bcfb3666946f510a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-06-15 11:42:22 +0200

    initial code

---
+++ src/cypress-terminal-error-render-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* global describe, it */
+const cypressTerminalErrorRender = require('.')
+
+describe('@cypress/cypress-terminal-error-render', () => {
+  it('write this test', () => {
+    console.assert(cypressTerminalErrorRender, 'should export something')
+  })
+})
